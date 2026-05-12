ae0964cc74b850c7d079c7c8793268f4048fd4dd
Kadir70-dev <kadirab1999@gmail.com>
2026-05-12 14:37:48 +0530

    checking

--- src/app/components/admin/AdminDashboard.tsx
+++ src/app/components/admin/AdminDashboard.tsx
@@ -259,6 +259,7 @@ export default function AdminDashboard() {
       name: "Gold Standard Dive",
       component:<GoldStandardDiveAdmin />
     },
+    
     // {
     //   name :" Environment Section",
     //   component :<EnvironmentSectionAdmin />
