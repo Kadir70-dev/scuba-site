8b549ec98925bc4fb64822ee77a04424273b8c7f
Kadir70-dev <kadirab1999@gmail.com>
2026-05-16 18:08:49 +0530

    feat(seo): optimize metadata, add sitemap.xml and robots.txt

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
+import {
+
+  HelmetProvider,
+
+} from "react-helmet-async";
+
 import App from "./app/App.tsx";
+
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+createRoot(
+  document.getElementById("root")!
+).render(
+
+  <HelmetProvider>
+
+    <BrowserRouter>
+
+      <App />
+
+    </BrowserRouter>
+
+  </HelmetProvider>
+
 );
